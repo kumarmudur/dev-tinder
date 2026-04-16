abeed3d70504990ec89a8be6761ab158ab6aa229
kumarmudur <kumarmudur10@gmail.com>
2026-04-16 07:29:24 +0530

    app changes

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome nodejs");
+});
+
+app.get('/hello', (req, res) => {
+   res.send("Hello hello!");
+});
+
+app.get('/test', (req, res)=> {
+   res.send('Hello from server');
+});
+
+app.listen(8080, () => {
+    console.log('server is successfully listening on port 8080');
+});
