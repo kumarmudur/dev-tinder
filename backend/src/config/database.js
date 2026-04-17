2591150fc65cd2415a9a3090b82a458cf252ec99
kumarmudur <kumarmudur10@gmail.com>
2026-04-18 04:56:42 +0530

    database changes

---
+++ backend/src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    await mongoose.connect(
+        "mongodb+srv://namastedev:nIl7y133A1Tb8gHC@namastenode.ziy6n6x.mongodb.net/devTinder"
+    );
+}
+
+module.exports = connectDB;
