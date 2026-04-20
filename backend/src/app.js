42dcd28c2539791fd38766529d3a6efe639b200f
kumarmudur <kumarmudur10@gmail.com>
2026-04-20 11:52:12 +0530

    get apis

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,38 @@ app.post('/signup', async (req, res) => {
     }
 });
 
+// Get user by email
+app.get('/user', async (req, res) => {
+   const userEmailId = req.body.emailId;
+
+   try {
+       const user = await User.findOne({ emailId: userEmailId});
+       if (!user) {
+           res.status(404).send('User not found.');
+       } else {
+           res.send(user);
+       }
+    // const users = await User.find({ emailId: userEmailId });
+    // if(users.length === 0) {
+    //     res.status(404).send('User not found.');
+    // } else {
+    //    res.send(users);
+    // }
+   } catch (error) {
+       console.log('Something went wrong', error);
+   }
+});
+
+// Feed API - GET / feed - get all the users from the database
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(404).send('Something went wrong' + error);
+    }
+});
+
 connectDB().then(() => {
     console.log("Connected to the database");
     app.listen(8080, () => {
