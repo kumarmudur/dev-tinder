597f69c5fef2840012822b82cbe849b9289cc58a
kumarmudur <kumarmudur10@gmail.com>
2026-04-21 18:12:02 +0530

    user auth included in apis

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 const adminAuth = (req, res, next) => {
     const token = "xyz";
     const isAdminAuthorized = token === 'xyz';
@@ -8,6 +11,34 @@ const adminAuth = (req, res, next) => {
     }
 }
 
+const userAuth =  async (req, res, next) => {
+  // read the token from the req cookies
+  // validate the token
+  // find the user
+
+    try {
+        const { token } = req.cookies || {};
+
+        if (!token) {
+            throw new Error('Token is not valid');
+        }
+
+        const decodedObj = await jwt.verify(token, 'DEV@Tinder$790');
+
+        const { _id } = decodedObj;
+
+        const user = await User.findById(_id);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).send('Unauthorized request' + error);
+    }
+};
+
 module.exports = {
     adminAuth,
+    userAuth
 }
