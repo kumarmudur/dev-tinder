a144afb5e0be1e5ee33758aaa9d81b34ca9c4ec2
kumarmudur <kumarmudur10@gmail.com>
2026-04-22 14:57:29 +0530

    profile related api changes

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -2,11 +2,29 @@ const express = require("express");
 const profileRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData} = require("../utils/validation");
 
-profileRouter.get('/profile', userAuth, async (req, res) => {
+profileRouter.get('/profile/view', userAuth, async (req, res) => {
     try {
         const user = req.user;
         res.send(user);
+    } catch (error) {
+        res.status(400).send('Error: ' + error.message);
+    }
+});
+
+profileRouter.patch('/profile/edit', userAuth, async (req, res) => {
+    try {
+       if(!validateEditProfileData(req)){
+           throw new Error("Invalid edit request");
+       }
+       const loggedInUser = req.user;
+       Object.keys(req.body).forEach(field => loggedInUser[field] = req.body[field]);
+       await loggedInUser.save();
+       res.json({
+           message: `${loggedInUser.firstName} your profile updated successfully.`,
+           data: loggedInUser,
+       });
     } catch (error) {
         res.status(400).send('Error: ' + error.message);
     }
