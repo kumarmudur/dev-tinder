1fdecc1e61029b7da9dd2b6e49faf2abdadca0f3
kumarmudur <kumarmudur10@gmail.com>
2026-06-27 07:27:54 -0400

    [frontend] Navbar changes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,32 +1,60 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+import { BASE_URL } from "../constants/common.js";
+import { API } from "../constants/api.js";
+import {removeUser} from "../utils/userSlice.js";
+
 const NavBar = () => {
+    const user = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+           await axios.post(`${BASE_URL}/${API.LOGOUT}`, { withCredentials: true });
+           dispatch(removeUser());
+           Cookies.remove("token");
+           navigate('/login');
+        } catch (error) {
+            // Error logic maybe redirect to error page
+            console.log(error);
+        }
+    };
+
     return (
         <div className="navbar bg-base-200 shadow-sm">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">👨‍💻DevTinder</a>
+                <Link to="/" className="btn btn-ghost text-xl">👨‍💻DevTinder</Link>
             </div>
-            <div className="flex gap-2">
-                <div className="dropdown dropdown-end mx-5">
+            { user && <div className="flex gap-2 mx-5 items-center">
+                <p className="px-4">Welcome, { user.firstName }</p>
+                <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                alt="user photo"
+                                src={user.photoUrl} />
                         </div>
                     </div>
                     <ul
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <li>
-                            <a className="justify-between">
+                            <Link to="/profile" className="justify-between">
                                 Profile
                                 <span className="badge">New</span>
-                            </a>
+                            </Link>
                         </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
+                        <li><Link to="/connections">Connections</Link></li>
+                        <li><Link to="/requests">Requests</Link></li>
+                        <li><Link to="/premium">Premium</Link></li>
+                        <li><a onClick={handleLogout}>Logout</a></li>
                     </ul>
                 </div>
-            </div>
+            </div>}
         </div>
     )
 };
