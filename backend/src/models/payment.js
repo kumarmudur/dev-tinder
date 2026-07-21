98d3d908c19604b74689d56e65933414491441c3
kumarmudur <kumarmudur10@gmail.com>
2026-07-21 09:25:12 -0400

    [backend] payment modal changes

---
+++ backend/src/models/payment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+        userId: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        paymentId: {
+            type: String,
+        },
+        orderId: {
+            type: String,
+            required: true,
+        },
+        status: {
+          type: String,
+          required: true,
+        },
+        amount: {
+            type: Number,
+            required: true,
+        },
+        currency: {
+            type: String,
+            required: true,
+        }
+    }
+)
