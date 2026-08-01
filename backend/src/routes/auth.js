9f4c5f34ae8f592dec1fea50f9a0895d9ce175f7
kumarmudur <kumarmudur10@gmail.com>
2026-08-01 01:20:33 -0500

    [backend] auth changes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require("../models/user");
-
 const { validateSignUpData } = require('../utils/validation');
+
 const authRouter = express.Router();
 
 authRouter.post('/signup', async (req, res) => {
@@ -23,8 +23,19 @@ authRouter.post('/signup', async (req, res) => {
             emailId,
             password: passwordHash,
         })
-        await user.save();
-        res.send('user added successfully.');
+        const savedUser = await user.save();
+        // create JWT token
+        const token = await savedUser.getJWT();
+
+        // the token to cookie and send the response back to the user
+        res.cookie('token', token, {
+            expires: new Date(Date.now() + 8 * 360000),
+        });
+
+        res.json({
+            message: 'user added successfully.',
+            data: savedUser,
+        });
     } catch (err) {
         res.status(400).send('Error: ' + err.message);
     }
@@ -48,15 +59,27 @@ authRouter.post('/login', async (req, res) => {
             res.cookie('token', token, {
                 expires: new Date(Date.now() + 8 * 360000),
             });
-            res.status(200).send('Login successful!');
+            res.status(200).send(user);
         } else {
             throw new Error('Invalid credentials');
         }
-        console.log(isPasswordValid);
-
     } catch (error) {
         res.status(400).send('Error: ' + error.message);
     }
+});
+
+authRouter.post('/logout', (req, res) => {
+    res.cookie('token', null, {
+        expires: new Date(0),
+    }).send('Logout successfully.');
+    // res.clearCookie('token', {
+    //     expires: new Date(0),
+    //     path: '/login',
+    //     httpOnly: true,
+    //     secure: false,      // true if HTTPS
+    //     sameSite: 'None',    // MUST match login
+    // });
+    // res.send('Logout successfully');
 });
 
 
